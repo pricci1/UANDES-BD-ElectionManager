ce5bee7cd06dd0b7e2e35b1f50e56f48282fa613
pricci1 <pricci1@miuandes.cl>
2017-11-04 17:53:43 -0400

    intento de usar DevExpress

---
+++ ElectionManager/InfoVotos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using DevExpress.XtraCharts;
+
+namespace ElectionManager
+{
+    public partial class InfoVotos : Form
+    {
+        public InfoVotos()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoVotos_Load(object sender, EventArgs e)
+        {
+            // Create an empty chart.
+            ChartControl pieChart = new ChartControl();
+
+            // Create a pie series.
+            Series series1 = new Series("A Pie Series", ViewType.Pie);
+
+            // Populate the series with points.
+            series1.Points.Add(new SeriesPoint("Russia", 17.0752));
+            series1.Points.Add(new SeriesPoint("Canada", 9.98467));
+            series1.Points.Add(new SeriesPoint("USA", 9.63142));
+            series1.Points.Add(new SeriesPoint("China", 9.59696));
+            series1.Points.Add(new SeriesPoint("Brazil", 8.511965));
+            series1.Points.Add(new SeriesPoint("Australia", 7.68685));
+            series1.Points.Add(new SeriesPoint("India", 3.28759));
+            series1.Points.Add(new SeriesPoint("Others", 81.2));
+
+            // Add the series to the chart.
+            pieChart.Series.Add(series1);
+
+            // Format the the series labels.
+            series1.Label.TextPattern = "{A}: {VP:p0}";
+
+            // Adjust the position of series labels. 
+            ((PieSeriesLabel)series1.Label).Position = PieSeriesLabelPosition.TwoColumns;
+
+            // Detect overlapping of series labels.
+            ((PieSeriesLabel)series1.Label).ResolveOverlappingMode = ResolveOverlappingMode.Default;
+
+            // Access the view-type-specific options of the series.
+            PieSeriesView myView = (PieSeriesView)series1.View;
+
+            // Show a title for the series.
+            myView.Titles.Add(new SeriesTitle());
+            myView.Titles[0].Text = series1.Name;
+
+            // Specify a data filter to explode points.
+            myView.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Value_1,
+                DataFilterCondition.GreaterThanOrEqual, 9));
+            myView.ExplodedPointsFilters.Add(new SeriesPointFilter(SeriesPointKey.Argument,
+                DataFilterCondition.NotEqual, "Others"));
+            myView.ExplodeMode = PieExplodeMode.UseFilters;
+            myView.ExplodedDistancePercentage = 30;
+            myView.RuntimeExploding = true;
+            myView.HeightToWidthRatio = 0.75;
+
+            // Hide the legend (if necessary).
+            pieChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+            // Add the chart to the form.
+            pieChart.Dock = DockStyle.Fill;
+            this.Controls.Add(pieChart);
+        }
+    }
+}
